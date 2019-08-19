297ba69f3631426d7c8a56ace079c1392ed083c3
Hentaitang <mz374427166@gmail.com>
2019-08-19 23:10:10 +0800

    feat:添加按钮组合

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import Button from './button'
-import Icon from './icon'
+import Vue from 'vue';
+import Button from './button';
+import Icon from './icon';
+import GroupButton from './button-group';
 
 Vue.component('lgm-button', Button);
 Vue.component('lgm-icon', Icon);
+Vue.component('lgm-button-group', GroupButton);
 
 new Vue({
   el: '#app',
@@ -14,13 +16,13 @@ new Vue({
   },
   methods:{
     changeLoading1(){
-      this.loading1 = !this.loading1
+      this.loading1 = !this.loading1;
     },
     changeLoading2(){
-      this.loading2 = !this.loading2
+      this.loading2 = !this.loading2;
     },
     changeLoading3(){
-      this.loading3 = !this.loading3
+      this.loading3 = !this.loading3;
     }
   }
 });
---
+++ src/button-group.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="lgm-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'lgm-button-group',
+    mounted(){
+      for(let node of this.$el.children){
+        if(node.tagName !== 'BUTTON'){
+          throw Error(`lgm-button-group 的子元素应该全是 lgm-button，不能包含${node.tagName.toLowerCase()}`)
+        }
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .lgm-button-group{
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    > .lgm-button{
+      border-radius: 0;
+      &:not(:first-child){
+        margin-left: -1px;
+      }
+      &:first-child{
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child{
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover{
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
