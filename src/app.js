4e030f200ea5e716542cdce6923d7d5fd4b9345f
Hentaitang <mz374427166@gmail.com>
2019-08-22 00:38:03 +0800

    feat:整合iconfont SVG 到本地 & 添加 input 组件

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
 import GroupButton from './button-group';
+import Input from './input';
 
-Vue.component('lgm-button', Button);
-Vue.component('lgm-icon', Icon);
-Vue.component('lgm-button-group', GroupButton);
+Vue.component(Button.name, Button);
+Vue.component(Icon.name, Icon);
+Vue.component(GroupButton.name, GroupButton);
+Vue.component(Input.name, Input);
 
 new Vue({
   el: '#app',
@@ -29,87 +31,3 @@ new Vue({
     }
   }
 });
-
-// import chai from 'chai';
-// import spies from 'chai-spies';
-// chai.use(spies);
-// const expect = chai.expect;
-// // 单元测试
-// {
-//   // 测试按钮含有 icon
-//   let Constructor = Vue.extend(Button);
-//   let vm = new Constructor({
-//     propsData: {
-//       icon: 'i-setting'
-//     }
-//   });
-//   vm.$mount();
-//   let useElement = vm.$el.querySelector('use');
-//   let href = useElement.getAttribute('xlink:href');
-//   expect(href).to.eq('#i-setting');
-//   vm.$el.remove();
-//   vm.$destroy()
-// }
-// {
-//   let constructor = Vue.extend(Button);
-//   let vm = new constructor({
-//     propsData: {
-//       icon: 'i-setting',
-//       loading: true
-//     }
-//   });
-//   vm.$mount();
-//   let useElement = vm.$el.querySelector('use');
-//   let href  = useElement.getAttribute('xlink:href');
-//   expect(href).to.eq('#i-loading');
-//   vm.$el.remove();
-//   vm.$destroy()
-// }
-// {
-//   const div = document.createElement('div');
-//   document.body.appendChild(div);
-//   let constructor = Vue.extend(Button);
-//   let vm = new constructor({
-//     propsData: {
-//       icon: 'i-setting'
-//     }
-//   });
-//   vm.$mount(div);
-//   let svg = vm.$el.querySelector('svg');
-//   let {order} = window.getComputedStyle(svg);
-//   expect(order).to.eq('0');
-//   vm.$el.remove();
-//   vm.$destroy()
-// }
-// {
-//   const div = document.createElement('div');
-//   document.body.appendChild(div);
-//   let constructor = Vue.extend(Button);
-//   let vm = new constructor({
-//     propsData: {
-//       icon: 'i-setting',
-//       iconPosition: 'right'
-//     }
-//   });
-//   vm.$mount(div);
-//   let svg = vm.$el.querySelector('svg');
-//   let {order} = window.getComputedStyle(svg);
-//   expect(order).to.eq('1');
-//   vm.$el.remove();
-//   vm.$destroy()
-// }
-// {
-//   let constructor = Vue.extend(Button);
-//   let vm = new constructor({
-//     propsData: {
-//       icon: 'i-setting'
-//     }
-//   });
-//   vm.$mount();
-//   let spy = chai.spy(function () {});
-//   vm.$on('click',spy);
-//   vm.$el.click();
-//   expect(spy).to.have.been.called();
-//   vm.$el.remove();
-//   vm.$destroy()
-// }
---
+++ src/input.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="lgm-input-wrapper">
+    <input class="lgm-input-inner" type="text" :placeholder="placeholder" :disabled="disabled">
+  </div>
+</template>
+<script>
+  export default {
+    name: 'lgm-input',
+    props: {
+      placeholder: {
+        type: String
+      },
+      disabled: {
+        type: Boolean
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .lgm-input-wrapper {
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 14px;
+
+    > .lgm-input-inner {
+      padding: 0 8px;
+      border-radius: 4px;
+      height: 32px;
+      line-height: 32px;
+      font-size: inherit;
+      border: 1px solid #ccc;
+      color: #606266;
+
+      &:hover {
+        border: 1px solid #aaa;
+      }
+
+      &:focus {
+        outline: none;
+        border-color: #777;
+      }
+
+      &[disabled] {
+        cursor: not-allowed;
+
+        &:hover {
+          border: 1px solid #ccc;
+        }
+      }
+
+      &::-webkit-input-placeholder {
+        color: #ccc;
+      }
+
+      &::-moz-placeholder {
+        color: #ccc;
+      }
+
+      &::-ms-input-placeholder {
+        color: #ccc;
+      }
+    }
+  }
+</style>
