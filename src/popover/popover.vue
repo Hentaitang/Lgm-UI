853866efd633e6dfe218397bef6df103f1931bc7
Hentaitang <mz374427166@gmail.com>
2019-08-28 15:01:48 +0800

    feat:添加popover样式

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lgm-popover">
-    <div class="contentWrapper" v-if="visible" ref="content">
+    <div class="contentWrapper" v-if="visible" ref="content" :class="{[position]: position}">
       <slot name="content"></slot>
     </div>
     <span ref="button" class="button" @click="clickButton">
@@ -11,35 +11,65 @@
 <script>
   export default {
     name: 'lgm-popover',
+    props: {
+      position: {
+        type: String,
+        default: 'top',
+        validator(value){
+          return ['top', 'left', 'right', 'bottom'].indexOf(value) >= 0
+        }
+      }
+    },
     data() {
       return {
         visible: false
       }
     },
     methods: {
-      close(){
+      popoverClose(){
         this.visible = false;
-        document.removeEventListener('click', this.onClickDocument)
+        document.removeEventListener('click', this.clickOutside)
+      },
+      popoverOpen(){
+        this.visible = true;
+        setTimeout(() => {
+          this.contentPosition();
+          document.addEventListener('click', this.clickOutside)
+        })
       },
       contentPosition(){
         document.body.appendChild(this.$refs.content);
+        const {content, button} = this.$refs;
         const {left, right, top, bottom} = this.$refs.button.getBoundingClientRect();
-        this.$refs.content.style.left = left + window.scrollX + 'px';
-        this.$refs.content.style.top = top + window.scrollY +'px';
-        console.log(left, right, top, bottom)
+        switch(this.position){
+          case 'top':
+            content.style.left = left + window.scrollX + 'px';
+            content.style.top = top + window.scrollY +'px';
+            break;
+          case 'left':
+            content.style.left = left + window.scrollX + 'px';
+            content.style.top = top + window.scrollY +'px';
+            break;
+          case 'right':
+            content.style.left = left + window.scrollX + 'px';
+            content.style.top = top + window.scrollY +'px';
+            break;
+          case 'bottom':
+            content.style.left = left + window.scrollX + 'px';
+            content.style.top = top + window.scrollY +'px';
+            break;
+        }
       },
       clickOutside($event){
         if(this.$refs.button && this.$refs.button.contains($event.target)) return;
         if(this.$refs.content && this.$refs.content.contains($event.target)) return;
-        this.close()
+        this.popoverClose()
       },
       clickButton() {
-        this.visible = !this.visible;
-        if (this.visible === true) {
-          setTimeout(() => {
-            this.contentPosition();
-            document.addEventListener('click', this.clickOutside)
-          })
+        if (this.visible === false) {
+          this.popoverOpen()
+        }else{
+          this.popoverClose()
         }
       }
     }
@@ -56,11 +86,33 @@
     }
   }
   .contentWrapper {
-    border: 1px solid red;
+    border: 1px solid black;
     position: absolute;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
     transform: translateY(-100%);
     background-color: white;
     margin-top: -10px;
+    padding: .8em 1em;
+    border-radius: 5px;
+    word-break: break-all;
+    max-width: 20em;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
+    &::before{
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      position: absolute;
+      top: 100%;
+      border-top-color: black;
+      border-bottom: none;
+    }
+    &::after{
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      position: absolute;
+      top: calc(100% - 1px);
+      border-top-color: white;
+      border-bottom: none;
+    }
   }
 </style>
